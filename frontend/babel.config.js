bc68eab32d340d0c209360118f87a5f771603ad8
Rafael Bortolozo <rafaelbortolozo@outlook.com>
2021-03-26 23:08:48 -0300

    commit

---
+++ frontend/babel.config.js
@@ -0,0 +1,6 @@
+module.exports= {
+    presets: [
+        '@babel/preset-env', //converte apenas as coisas que o browser ainda não entende
+        '@babel/preset-react' //converte código do react
+    ]
+}
