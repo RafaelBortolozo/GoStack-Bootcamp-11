2e2de70c203b85914ef7838502ee16d82fb066f5
Rafael Bortolozo <rafaelbortolozo@outlook.com>
2021-04-04 18:34:36 -0300

    State concept

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header'
 
 export default function App(){
-    return ( 
-        <Header title="Homepage">
+    const [projects, setProjects]= useState(['text1', 'text2'])
+    
+    // useState retorna um array com 2 posições
+    // 1. Os dados
+    // 2. função de atualização dos dados
+
+    function handleAddProject() {    // dica do Diego: quando há uma função acionada pelo usuario, inicia-se o nome dela com "handle" 
+
+        //para alterar a lista de projetos, devemos usar o setProjects
+        setProjects([...projects, `Novo projeto ${Date.now()}`])
+
+        console.log(projects)
+    }
+
+    return (
+        <> 
+            <Header title="Projects" />
+
             <ul>
-                <li>item 1</li>
-                <li>item 2</li>
-                <li>item 3</li>
+                {projects.map(project => <li key={project}>{project}</li>)}  
             </ul>
-        </Header>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+        </>
     )
 }
 
