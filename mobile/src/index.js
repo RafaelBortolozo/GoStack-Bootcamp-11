5b22681a3cd3e96286fe3cbbfbcf134e4c6d7590
Rafael Bortolozo <rafaelbortolozo@outlook.com>
2021-04-11 00:55:16 -0300

    First screen with "Hello world"

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,27 @@
 import React from 'react'; //obrigatório para usar JSX
-import { View } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
 
 export default function App(){
-  return <View />
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor={styles.container.backgroundColor}/>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello World</Text>
+      </ View>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center', //Centralização vertical
+    alignItems: 'center'      //Centralização horizontal
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 25,
+    fontWeight: 'bold'
+  }
+})
