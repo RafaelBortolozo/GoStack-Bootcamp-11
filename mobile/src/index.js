e1a738d739c97315883135a4077228e84deff53d
Rafael Bortolozo <rafaelbortolozo@outlook.com>
2021-04-11 16:45:29 -0300

    mobile connected with the API

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,31 @@
-import React from 'react'; //obrigatório para usar JSX
-import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import React, { useEffect, useState } from 'react'; //obrigatório para usar JSX
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import api from './services/api';
 
 export default function App(){
+  const [projects, setProjects] = useState([])
+
+  useEffect(()=>{
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
+  
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor={styles.container.backgroundColor}/>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello World</Text>
-      </ View>
+      
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}  //array dos elementos da lista
+          keyExtractor={project => project.id}  //arrow function que retorna algo unico
+                                                //de cada elemento da lista, no caso o id
+          
+          renderItem={({ item: project }) => ( //arrow function que retorna cada elemento do array
+            <Text style={styles.project}> {project.title} </Text>
+          )}  
+        />
+      </SafeAreaView>
     </>
   )
 }
@@ -15,13 +33,10 @@ export default function App(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7159c1',
-    justifyContent: 'center', //Centralização vertical
-    alignItems: 'center'      //Centralização horizontal
+    backgroundColor: '#7159c1'
   },
-  title: {
+  project: {
     color: '#FFF',
-    fontSize: 25,
-    fontWeight: 'bold'
+    fontSize: 25
   }
 })
